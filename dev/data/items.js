aa12abc98649bd02a1b5cfab6830471a7f5cc88f
Shae Kuronen <shae.kuronen@gmail.com>
2014-04-19 08:45:29 -0700

    made img links relative

--- dev/data/items.js
+++ dev/data/items.js
@@ -4,8 +4,8 @@ App.Data = [
     "id": 1,
     "title": "Item1 Title",
     "description": "The description for item 1 goes here",
-    "thumbnail": "/img/items/item01_thumbnail.jpg",
-    "item_detail_image": "/img/items/item01_thumbnail.jpg",
+    "thumbnail": "img/items/item01_thumbnail.jpg",
+    "item_detail_image": "img/items/item01_thumbnail.jpg",
     "categories": [
       "cat1",
       "cat2",
@@ -16,8 +16,8 @@ App.Data = [
     "id": 2,
     "title": "Item2 Title",
     "description": "The description for item 2 goes here",
-    "thumbnail": "/img/items/item02_thumbnail.jpg",
-    "item_detail_image": "/img/items/item02_thumbnail.jpg",
+    "thumbnail": "img/items/item02_thumbnail.jpg",
+    "item_detail_image": "img/items/item02_thumbnail.jpg",
     "categories": [
       "cat4",
       "cat5",
@@ -28,8 +28,8 @@ App.Data = [
     "id": 3,
     "title": "Item3 Title",
     "description": "The description for item 3 goes here",
-    "thumbnail": "/img/items/item03_thumbnail.jpg",
-    "item_detail_image": "/img/items/item03_thumbnail.jpg",
+    "thumbnail": "img/items/item03_thumbnail.jpg",
+    "item_detail_image": "img/items/item03_thumbnail.jpg",
     "categories": [
       "cat1",
       "cat3"
@@ -39,8 +39,8 @@ App.Data = [
     "id": 4,
     "title": "Item4 Title",
     "description": "The description for item 4 goes here",
-    "thumbnail": "/img/items/item04_thumbnail.jpg",
-    "item_detail_image": "/img/items/item04_thumbnail.jpg",
+    "thumbnail": "img/items/item04_thumbnail.jpg",
+    "item_detail_image": "img/items/item04_thumbnail.jpg",
     "categories": [
       "cat2",
       "cat4"
@@ -50,8 +50,8 @@ App.Data = [
     "id": 5,
     "title": "Item5 Title",
     "description": "The description for item 5 goes here",
-    "thumbnail": "/img/items/item05_thumbnail.jpg",
-    "item_detail_image": "/img/items/item05_thumbnail.jpg",
+    "thumbnail": "img/items/item05_thumbnail.jpg",
+    "item_detail_image": "img/items/item05_thumbnail.jpg",
     "categories": [
       "cat5",
       "cat6"
@@ -61,8 +61,8 @@ App.Data = [
     "id": 6,
     "title": "Item6 Title",
     "description": "The description for item 6 goes here",
-    "thumbnail": "/img/items/item06_thumbnail.jpg",
-    "item_detail_image": "/img/items/item06_thumbnail.jpg",
+    "thumbnail": "img/items/item06_thumbnail.jpg",
+    "item_detail_image": "img/items/item06_thumbnail.jpg",
     "categories": [
       "cat1",
       "cat6"
@@ -72,8 +72,8 @@ App.Data = [
     "id": 7,
     "title": "Item7 Title",
     "description": "The description for item 7 goes here",
-    "thumbnail": "/img/items/item07_thumbnail.jpg",
-    "item_detail_image": "/img/items/item07_thumbnail.jpg",
+    "thumbnail": "img/items/item07_thumbnail.jpg",
+    "item_detail_image": "img/items/item07_thumbnail.jpg",
     "categories": [
       "cat2",
       "cat4"
@@ -83,8 +83,8 @@ App.Data = [
     "id": 8,
     "title": "Item8 Title",
     "description": "The description for item 8 goes here",
-    "thumbnail": "/img/items/item08_thumbnail.jpg",
-    "item_detail_image": "/img/items/item08_thumbnail.jpg",
+    "thumbnail": "img/items/item08_thumbnail.jpg",
+    "item_detail_image": "img/items/item08_thumbnail.jpg",
     "categories": [
       "cat3",
       "cat5"
@@ -94,8 +94,8 @@ App.Data = [
     "id": 9,
     "title": "Item9 Title",
     "description": "The description for item 9 goes here",
-    "thumbnail": "/img/items/item09_thumbnail.jpg",
-    "item_detail_image": "/img/items/item09_thumbnail.jpg",
+    "thumbnail": "img/items/item09_thumbnail.jpg",
+    "item_detail_image": "img/items/item09_thumbnail.jpg",
     "categories": [
       "cat4",
       "cat6"
@@ -105,8 +105,8 @@ App.Data = [
     "id": 10,
     "title": "Item10 Title",
     "description": "The description for item 10 goes here",
-    "thumbnail": "/img/items/item10_thumbnail.jpg",
-    "item_detail_image": "/img/items/item10_thumbnail.jpg",
+    "thumbnail": "img/items/item10_thumbnail.jpg",
+    "item_detail_image": "img/items/item10_thumbnail.jpg",
     "categories": [
       "cat1",
       "cat4"
