bdad2cdb4ff25a693b920a1b1b1c312482ab992c
Shae Kuronen <shae.kuronen@gmail.com>
2014-04-21 12:45:16 -0700

    added project thumbnails

---
+++ dev/data/projects.js
@@ -0,0 +1,162 @@
+
+App.Projects = [
+  {
+    "id": 12,
+    "title": "Denizen Site",
+    "description": "The description goes here",
+    "thumbnail": "/img/projects/denizen_site/thumbnail.jpg",
+    "item_detail_image": "/img/items/item01_thumbnail.jpg",
+    "categories": [
+      "Landing",
+      "Responsive"
+    ],
+    "client": "Denizen",
+    "client_logo": ""
+  },
+  {
+    "id": 11,
+    "title": "Responsive + Mobile Optimized 206 Site",
+    "description": "The description goes here",
+    "thumbnail": "/img/projects/206_site/thumbnail.jpg",
+    "item_detail_image": "/img/items/item02_thumbnail.jpg",
+    "categories": [
+      "Landing",
+      "Responsive",
+      "Mobile Optimized"
+    ],
+    "client": "206inc",
+    "client_logo": ""
+  },
+  {
+    "id": 10,
+    "title": "Toyota Rav4 EV Tour Site",
+    "description": "The description goes here",
+    "thumbnail": "/img/projects/toyota_rav4_ev/thumbnail.jpg",
+    "item_detail_image": "/img/items/item03_thumbnail.jpg",
+    "categories": [
+      "Landing"
+    ],
+    "client": "Toyota",
+    "client_logo": ""
+  },
+  {
+    "id": 9,
+    "title": "DoubleDown Interactive Site",
+    "description": "New Responsive + Mobile Optimized DoubleDown Site",
+    "thumbnail": "/img/projects/doubledown_site/thumbnail.jpg",
+    "item_detail_image": "/img/items/item04_thumbnail.jpg",
+    "categories": [
+      "Landing",
+      "Responsive",
+      "Mobile Optimized",
+      "CMS"
+    ],
+    "client": "DoubleDown Interactive",
+    "client_logo": ""
+  },
+  {
+    "id": 8,
+    "title": "Kindle Fire + Marvel Facebook App",
+    "description": "The description goes here",
+    "thumbnail": "/img/projects/kindle_marvel/thumbnail.jpg",
+    "item_detail_image": "/img/items/item05_thumbnail.jpg",
+    "categories": [
+      "Landing",
+      "Facebook",
+      "Mobile Optimized"
+    ],
+    "client": "Kindle Fire",
+    "client_logo": ""
+  },
+  {
+    "id": 7,
+    "title": "Coca-Cola Unbottled Blog",
+    "description": "The description goes here",
+    "thumbnail": "/img/projects/coca_cola_unbottled/thumbnail_1.jpg",
+    "item_detail_image": "/img/items/item06_thumbnail.jpg",
+    "categories": [
+      "Landing",
+      "Responsive",
+      "CMS"
+    ],
+    "client": "",
+    "client_logo": ""
+  },
+  {
+    "id": 6,
+    "title": "Alexander Wang Facebook App",
+    "description": "The description goes here",
+    "thumbnail": "/img/projects/alexander_wang/thumbnail.jpg",
+    "item_detail_image": "/img/items/item07_thumbnail.jpg",
+    "categories": [
+      "Landing",
+      "Facebook"
+    ],
+    "client": "Alexander Wang",
+    "client_logo": ""
+  },
+  {
+    "id": 5,
+    "title": "Jack Spade Facebook App",
+    "description": "The description goes here",
+    "thumbnail": "/img/projects/jack_spade/thumbnail.jpg",
+    "item_detail_image": "/img/items/item08_thumbnail.jpg",
+    "categories": [
+      "Landing",
+      "Facebook"
+    ],
+    "client": "Jack Spade",
+    "client_logo": ""
+  },
+  {
+    "id": 4,
+    "title": "Buddy Media Site",
+    "description": "The description goes here",
+    "thumbnail": "/img/projects/buddy_media/thumbnail.jpg",
+    "item_detail_image": "/img/items/item09_thumbnail.jpg",
+    "categories": [
+      "Landing"
+    ],
+    "client": "Buddy Media",
+    "client_logo": ""
+  },
+  {
+    "id": 3,
+    "title": "Kate Spade Facebook App",
+    "description": "The description goes here",
+    "thumbnail": "/img/projects/kate_spade/thumbnail.jpg",
+    "item_detail_image": "/img/items/item10_thumbnail.jpg",
+    "categories": [
+      "Landing",
+      "Facebook"
+    ],
+    "client": "",
+    "client_logo": ""
+  },
+  {
+    "id": 2,
+    "title": "Americana Manhasset Tumblr",
+    "description": "The description goes here",
+    "thumbnail": "/img/projects/americana_manhasset/thumbnail.jpg",
+    "item_detail_image": "/img/items/item10_thumbnail.jpg",
+    "categories": [
+      "Landing",
+      "Tumblr"
+    ],
+    "client": "Americana Manhasset",
+    "client_logo": ""
+  },
+  {
+    "id": 1,
+    "title": "Neiman Marcus Tumblr",
+    "description": "The description goes here",
+    "thumbnail": "/img/projects/neiman_marcus/thumbnail.jpg",
+    "item_detail_image": "/img/items/item10_thumbnail.jpg",
+    "categories": [
+      "Landing",
+      "Tumblr"
+    ],
+    "client": "Neiman Marcus",
+    "client_logo": ""
+  }
+];
