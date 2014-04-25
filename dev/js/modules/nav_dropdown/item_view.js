d689b933e0e53dc2558b1012e31be7f5768b85c6
Shae Kuronen <shae.kuronen@gmail.com>
2014-04-25 08:28:02 -0700

    added click handlers to nav dropdown item view

--- dev/js/modules/nav_dropdown/item_view.js
+++ dev/js/modules/nav_dropdown/item_view.js
@@ -4,16 +4,21 @@ App.NavDropdownItemView = Marionette.ItemView.extend({
   template: JST['dev/templates/dropdown_item.hbs'],
 
   events: {
-    'click .sk-dropdown-nav_dropdown-element li': 'projectsCategorySelected',
-    'click .sk-dropdown-nav_dropdown-element_sub-nav li': 'projectDetailSelected'
+    'click li h4': 'projectsCategorySelected',
+    'click .sk-dropdown-nav_dropdown-element_sub-nav li': 'projectSelected'
   },
 
-  projectsCategorySelected: function(thing) {
-    console.log('this thing should be available now');
-    window.thing = thing;
+  projectsCategorySelected: function(event) {
+    console.log('in projectsCategorySelected, the category selected is ' + event.currentTarget.innerHTML);
     // notify app that a projects category has been selected
-    // App.vent.trigger('projects:category:selected', _category);
+    App.vent.trigger('projects:category:selected', event.currentTarget.innerHTML);
 
+  },
+
+  projectSelected: function(event) {
+    event.stopPropagation();
+    console.log('in projectSelected, the category selected is ' + event.currentTarget.innerHTML);
+    App.vent.trigger('projects:selected', event.currentTarget.innerHTML);
   }
 
 });
