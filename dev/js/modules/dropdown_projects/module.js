910fcb9c2870082f675a9501a00f38ec8cd9bb5c
Shae Kuronen <shae.kuronen@gmail.com>
2014-05-07 07:00:51 -0700

    first working version of dropdown using itemviews for all nav items

--- dev/js/modules/dropdown_projects/module.js
+++ dev/js/modules/dropdown_projects/module.js
@@ -5,78 +5,42 @@ App.module("ProjectsDropdown", function(ProjectsDropdown, App, Backbone, Marione
 
     tagName: 'li',
 
-    template: '<span>{{this}}</span>',
-
-    events: {
-
-    }
+    template: JST['dev/templates/projects_dropdown_project_item.hbs']
 
   });
 
-  ProjectsDropdown.ProjectsCollectionView = Marionette.CollectionView.extend({
-
-    tagName: 'ul',
-
-    itemView: ProjectsDropdown.ProjectItemView,
-
-    initialize: function() {
-
-    }
-
-  });
-
-  // ProjectsDropdown.CategoryCompositeView = Marionette.CompositeView.extend({
-
-  //   tagName: 'li',
-
-  //   itemView: ProjectsDropdown.ProjectsCollectionView,
-
-  //   itemViewContainer: '.sk-dropdown-nav_dropdown-element_sub-nav',
-
-  //   template: JST['dev/templates/projects_dropdown_category_item.hbs'],
-
-  //   initialize: function() {
-
-  //   }
-
-  // });
-
-  // ProjectsDropdown.CategoriesCompositeView = Marionette.CompositeView.extend({
-
-  //   el: '#sk-projects-dropdown-element',
-
-  //   itemView: ProjectsDropdown.CategoryCompositeView,
-
-  //   itemViewContainer: '.sk-dropdown-nav_dropdown-element_sub-nav',
-
-  //   initialize: function() {
-  //     console.log('App.ProjectsDropdownCategoriesCompositeView initialize happened');
-  //   }
-
-  // });
-
-  ProjectsDropdown.CategoriesCompositeView = Marionette.CompositeView.extend({
+  ProjectsDropdown.CategoryCompositeView = Marionette.CompositeView.extend({
 
     tagName: 'li',
 
-    itemView: ProjectsDropdown.ProjectsCollectionView,
+    // itemView: ProjectsDropdown.ProjectsCollectionView,
+    itemView: ProjectsDropdown.ProjectItemView,
 
     itemViewContainer: '.sk-dropdown-nav_dropdown-element_sub-nav',
 
     template: JST['dev/templates/projects_dropdown_category_item.hbs'],
 
-    initialize: function() {
-
+    initialize: function(options) {
+      // set this view's collection to App.categoriesCollection.projects
+      this.collection = options.model.get('projects');
     }
 
   });
 
-  // instantiate projects dropdown item view
-  ProjectsDropdown.categoriesCompositeView = new ProjectsDropdown.CategoriesCompositeView({
+  ProjectsDropdown.CategoriesCollectionView = Marionette.CollectionView.extend({
+
+    el: '#sk-projects-dropdown-element',
+
+    itemView: ProjectsDropdown.CategoryCompositeView
+
+  });
+
+  // instantiate projects dropdown collection view
+    ProjectsDropdown.categoriesCollectionView = new ProjectsDropdown.CategoriesCollectionView({
     collection: App.categoriesCollection
   });
 
-  // render projects dropdown item view
-  ProjectsDropdown.categoriesCompositeView.render();
+  // render projects dropdown collection view
+  ProjectsDropdown.categoriesCollectionView.render();
 
 });
--- dev/templates/compiled.js
+++ dev/templates/compiled.js
@@ -134,6 +134,19 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
   if (helper = helpers.title) { stack1 = helper.call(depth0, {hash:{},data:data}); }
   else { helper = (depth0 && depth0.title); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
   buffer += escapeExpression(stack1)
-    + "</h4>\n\n<ul class='sk-dropdown-nav_dropdown-element_sub-nav'></ul>\n";
+    + "</h4>\n\n<ul id=\"sk-projects-dropdown-sub-element\" class='sk-dropdown-nav_dropdown-element_sub-nav'></ul>\n";
+  return buffer;
+  });
+
+this["JST"]["dev/templates/projects_dropdown_project_item.hbs"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
+  var buffer = "", stack1, helper, functionType="function", escapeExpression=this.escapeExpression;
+
+
+  if (helper = helpers.title) { stack1 = helper.call(depth0, {hash:{},data:data}); }
+  else { helper = (depth0 && depth0.title); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
+  buffer += escapeExpression(stack1)
+    + "\n";
   return buffer;
   });
