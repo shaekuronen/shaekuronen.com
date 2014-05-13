aa01b5fda2f5c36c5963527cab812c3c9d617280
Shae Kuronen <shae.kuronen@gmail.com>
2014-05-13 07:09:24 -0700

    working on model visible state detection logic

--- dev/js/modules/main_content/controller.js
+++ dev/js/modules/main_content/controller.js
@@ -9,6 +9,21 @@ App.MainContentController = Marionette.Controller.extend({
 
       console.log('App.MainContentController change:visible event happened');
 
+      // get models that are not visible: false
+      var _models = this.collection.getVisibleModels();
+
+      console.log('this.collection.getVisibleModels() models are ' + _models);
+      window._models = _models;
+
+      // get models state
+      // note: currently assuming all models not visible: false models will have state thumbnail OR detail
+      // but will need to add testing for multiple states + error handling
+      // so TEMPORARILY querying only zeroth model, to only return first models state
+      var _state = _models.at(0).get('visible');
+
+      console.log('model zero state is ' + _state);
+
+
     });
 
   }
